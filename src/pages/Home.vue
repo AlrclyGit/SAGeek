35605f8b7ff0d0bb549545381ef77a5ac4b0d115
AlrclySuzuki <alrcly@foxmail.com>
2022-03-23 17:06:10 +0800

    Vue3 的代码组织方式

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,1 +1,32 @@
-<template>Home</template>
+<template>
+    <div>
+        <h1 @click="add">{{ count }}</h1>
+        <TodoList />
+    </div>
+</template>
+
+<script setup>
+import TodoList from "../components/TodoList.vue"
+import {useMouse} from '../utils/mouse'
+
+import { ref } from "vue";
+
+let {x,y} = useMouse()
+
+let { count, add } = lejia()
+
+function lejia() {
+    const count = ref(1)
+    function add() {
+        count.value++
+    }
+    return { count, add };
+}
+
+</script>
+
+<style>
+h1 {
+    color: red;
+}
+</style>
